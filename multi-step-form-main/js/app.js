7f217b3b136eee6b3b4b472a3781066d94696ff1
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2022-12-13 23:49:52 +0630

    render steps

--- multi-step-form-main/js/app.js
+++ multi-step-form-main/js/app.js
@@ -1,45 +1,76 @@
-
 /** Content Card Mobile Header Component start */
-function createContentCardMobileHeader() {
-  const steps = {
-    1: {
+function setUpContentCardMobileHeader() {
+  const steps = [
+    {
       title: "Personal  Info",
       desc: "Please provide your name, email address, and phone number.",
     },
-    2: {
+    {
       title: "Select Your Plan",
       desc: "You have the option of monthly or yearly billing.",
     },
-    3: {
+    {
       title: "Pick add-ons",
       desc: "Add-ons help enhance your gaming experience.",
     },
-    4: {
+    {
       title: "Finishing up",
       desc: "Double-check everything looks OK before confirming.",
     },
-  };
+  ];
   const contentCardMobileHeader = document.querySelector(".content-header");
   const contentCardMobileHeaderTitle =
     contentCardMobileHeader.querySelector("h1");
   const contentCardMobileHeaderDesc =
     contentCardMobileHeader.querySelector("p");
   const publicAPI = {
-    activeStep: 1,
+    activeStep: 0,
     setActiveStep(index) {
-        this.activeStep  = index;
-        this.render();
+      this.activeStep = index;
+      this.render();
     },
     render() {
-        contentCardMobileHeaderTitle.textContent = steps[this.activeStep].title;
-        contentCardMobileHeaderDesc.textContent = steps[this.activeStep].desc;
-    }
-  }
+      contentCardMobileHeaderTitle.textContent = steps[this.activeStep].title;
+      contentCardMobileHeaderDesc.textContent = steps[this.activeStep].desc;
+    },
+  };
 
   return publicAPI;
 }
-
-const contentCardMobile = createContentCardMobileHeader();
-
+const contentCardMobile = setUpContentCardMobileHeader();
+contentCardMobile.render();
 /** Content Card Mobile Header Component End */
 
+/** Mobile Steps start */
+function setUpMobileSteps() {
+  const mobileSteps = document.querySelectorAll(".steps-mobile ul li");
+  const publicAPI = {
+    activeStep: 0,
+    setActiveStep(index) {
+      this.activeStep = index;
+      this.render();
+    },
+    render() {
+      mobileSteps.forEach((stepLi, i) => {
+        stepLi.classList.remove("active");
+        if (this.activeStep == i ) {
+          stepLi.classList.add("active");
+        }
+      });
+    },
+  };
+  return publicAPI;
+}
+
+const mobileSteps = setUpMobileSteps();
+mobileSteps.render();
+/** Mobile Steps End */
+
+/** Action Triggers */
+const nextBtn = document.getElementById("next");
+nextBtn.addEventListener("click", () => {
+  console.log(contentCardMobile.activeStep + 1);
+  console.log(mobileSteps.activeStep + 1);
+  contentCardMobile.setActiveStep(contentCardMobile.activeStep + 1);
+  mobileSteps.setActiveStep(mobileSteps.activeStep + 1);
+});
